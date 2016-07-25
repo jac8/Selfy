527932518cb00a1702a4f1c3dbe15c1a70db2c85
jac8 <mrjac9@gmail.com>
2016-07-26 01:45:45 +0430

    Update poker.lua

--- plugins/poker.lua
+++ plugins/poker.lua
@@ -2,7 +2,9 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = '🌹😐️'
+local text = '😐
+👕
+👖️'
 if matches[1] == '😐' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
