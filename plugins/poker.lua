12590abdf6aceffc2234f1750369a3cc50d30a2b
jac8 <mrjac9@gmail.com>
2016-07-30 15:15:41 +0430

    Update poker.lua

--- plugins/poker.lua
+++ plugins/poker.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'کی !من 😐️'
+local text = 'کی من ؟ 😐️'
 if matches[1] == 'دیلان' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
